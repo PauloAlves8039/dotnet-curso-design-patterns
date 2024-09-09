b3b305295ea181a3bcc966fe83565cde3fda2590
PauloAlves8039 <pj38alves@gmail.com>
2024-09-09 09:55:44 -0300

    feat: versão inicial do projeto

---
+++ src/DesignPatterns.Curso/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("********** Curso Design Patterns **********");
+
+
+        Console.ReadKey();
+    }
+}
